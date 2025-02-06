31f65100b373428d3939b54e874e083b0a6ac928
Thiago Ribeiro <tilmaisesseemail@gmail.com>
2025-02-06 00:37:04 +0000

    Desafio aventureiro completo

--- DesafioAventureiro.c
+++ DesafioAventureiro.c
@@ -4,9 +4,38 @@ int main(){
 
     // Declaração das variáveis e o valor das que as estruturas de repetição não tem inicialização
     int cavalo1, cavalo2, cavalo3 = 1, cavalo4 = 1;
+    int bispo = 1;
+    int torre = 1;
+    int rainha = 1;
 
-    // Imprimir a primeira movimentação
-    printf("Primeira movimentação do cavalo:\n");
+    // Imprimir a movimentação do bispo.
+    printf("Movimentação do bispo:\n");
+    // Estrutura de repetição para fazer o movimento das peças de forma repetida ate cumprir a condição.
+    while(bispo <= 5){
+        
+        printf("Direita, Cima\n");
+        
+        bispo++;
+    }
+    // Imprimir a movimentação da torre.
+    printf("\nMovimentação da Torre:\n");
+    // Estrutura de repetição para fazer o movimento das peças de forma repetida ate cumprir a condição.
+    while(torre <=5){
+        
+        printf("Direita\n");
+        torre++;
+    }
+    // Imprimir a movimentação da rainha
+    printf("\nMovimentação da rainha:\n");
+    // Estrutura de repetição para fazer o movimento das peças de forma repetida ate cumprir a condição.
+    while(rainha <= 8){
+
+        printf("Esquerda\n");
+        rainha++;
+    }
+
+    // Imprimir a primeira movimentação do cavalo
+    printf("\nPrimeira movimentação do cavalo:\n");
 
     // Primeira estrutura de repetição aninhada da primeira movimentação do cavalo
     for( cavalo1 = 1; cavalo1 <= 1; cavalo1++){
@@ -17,7 +46,7 @@ int main(){
 
     }
     
-    // Imprimir a segunda movimentação
+    // Imprimir a segunda movimentação do cavalo
     printf("\nSegunda movimentação do cavalo\n");
 
     // Segunda estrutura de repetição aninhada da segunda movimentação do cavalo
@@ -29,6 +58,5 @@ int main(){
             cavalo4++;
         }
     }
-    
     return 0;
 }
